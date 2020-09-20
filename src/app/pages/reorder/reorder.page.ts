f0de1f08dd6062df8f417bffc0cb9f5dd24fc611
zeleandroxavier <zeleandroxavier@gmail.com>
2020-09-20 12:51:38 -0300

    reorder

--- src/app/pages/reorder/reorder.page.ts
+++ src/app/pages/reorder/reorder.page.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-reorder',
   templateUrl: './reorder.page.html',
   styleUrls: ['./reorder.page.scss'],
 })
-export class ReorderPage implements OnInit {
+export class ReorderPage {
+ 
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
-  doReorder(ev: any){
+  doReorder(ev: any) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
     console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
-    ev.dentail.complete();
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
   }
 
-  togleReorderGroup(){
-    this.reorderGroup.disable = !this.togleReorderGroup.disabled;
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
   }
-
 }
